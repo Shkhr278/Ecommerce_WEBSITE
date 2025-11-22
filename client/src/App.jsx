cd9c2ab20ca8f418dd17ef4be5d2040cf5ffd5bf
SHIKHAR SINGH <shikhars.singh27@gmail.com>
2025-11-22 17:54:23 +0530

    site is booting successfully now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import ProductsPage from "@/pages/events";
 import CartPage from "@/pages/cart";
 import FavoritesPage from "@/pages/favorites";
@@ -23,7 +24,7 @@ function Router() {
   );
 }
 
-function App() {
+export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
@@ -33,5 +34,3 @@ function App() {
     </QueryClientProvider>
   );
 }
-
-export default App;
